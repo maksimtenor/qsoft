2ce432290891bb23c8fb93139da71a09c8275ac1
marevich <maksimrozette@gmail.com>
2021-10-01 19:54:44 +0300

    update 01.10.2021 time result 7:hours

---
+++ main.js
@@ -0,0 +1,6 @@
+if($(window).width() < 760 ){
+    $(document).ready(function() {
+        $('#search').appendTo('.wrapper-header');
+        $('.header__user__border-left').appendTo('')
+    });    
+}
